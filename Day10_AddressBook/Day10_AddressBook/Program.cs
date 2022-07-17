82582e0b4ab73eb771eb90f56e46d7b8f23cac65
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-07-17 18:53:23 +0530

    [pavan] Unique name for address book

--- Day10_AddressBook/Day10_AddressBook/Program.cs
+++ Day10_AddressBook/Day10_AddressBook/Program.cs
@@ -1,36 +1,60 @@
 ﻿using Day10_AddressBook;
 
-AddressBook address = new AddressBook();
 
+AddressBook address = new AddressBook();
 Console.WriteLine("Welcome to Address Book : ");
 
-string command = "";
+UniqueName UniqueName = new UniqueName();
+string command1 = "";
 
-while (command != "exit")
+while (command1 != "exit")
 {
-    Console.WriteLine("Please enter a command: add , edit, remove, display, multipleperson, exit :");
-    command = Console.ReadLine().ToLower();
+    Console.WriteLine("enter the Number : 1. for Add AdressBook, 2. for Edit AddressBook, 3.for exit: ");
+    int option = Convert.ToInt32(Console.ReadLine());
 
-    switch (command)
+    switch (option)
     {
-        case "add":
-            address.AddPerson();
+        case 1:
+            UniqueName.AddAddressBook();
             break;
-        case "edit":
-            address.EditContact();
-            break;
-        case "remove":
-            address.RemoveContact();
-            break;
-        case "display":
-            address.Display();
-            break;
-        case "multipleperson":
-            address.AddMultipleContact();
+        case 2:
+            Console.WriteLine("enter the addressbook name you want to edit");
+            string addressBookName = Console.ReadLine();
+            if (UniqueName.adressBooks.ContainsKey(addressBookName.ToLower()))
+            {
+                string command = "";
+                while (command != "exit")
+                {
+                    Console.WriteLine("Please enter a command: add , edit, remove, display, multipleperson, exit :");
+                    command = Console.ReadLine().ToLower();
+
+                    switch (command)
+                    {
+                        case "add":
+                            address.AddPerson();
+                            break;
+                        case "edit":
+                            address.EditContact();
+                            break;
+                        case "remove":
+                            address.RemoveContact();
+                            break;
+                        case "display":
+                            address.Display();
+                            break;
+                        case "multipleperson":
+                            address.AddMultipleContact();
+                            break;
+                        default:
+                            break;
+
+                    }
+                }
+            }
             break;
         default:
+            command1 = "exit";
             break;
 
     }
-
 }
---
+++ Day10_AddressBook/Day10_AddressBook/UniqueName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_AddressBook
+{
+    public class UniqueName
+    {
+        public Dictionary<string, AddressBook> adressBooks = new Dictionary<string, AddressBook>();
+        public void AddAddressBook()
+        {
+            AddressBook adressBook = new AddressBook();
+            adressBook.AddMultipleContact();
+            Console.WriteLine("enter the addressbook name");
+            string addressName = Convert.ToString(Console.ReadLine());
+            adressBooks.Add(addressName.ToLower(), adressBook);
+
+        }
+    }
+}
